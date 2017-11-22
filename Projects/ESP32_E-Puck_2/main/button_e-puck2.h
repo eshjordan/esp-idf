ecbd95de8239d58bf0e33e4615b9de38e6948341
Eliot Ferragni <eliot.ferragni@epfl.ch>
2017-11-22 10:33:13 +0100

    -BUTTON : added first functions to handle the button

---
+++ Projects/ESP32_E-Puck_2/main/button_e-puck2.h
@@ -0,0 +1,32 @@
+/*
+
+File    : button_E-Puck_2.h
+Author  : Eliot Ferragni
+Date    : 22 november 2017
+REV 1.0
+
+Functions to configure and use the button through interruption
+*/
+
+
+#ifndef BUTTON_E_PUCK2_H
+#define BUTTON_E_PUCK2_H
+
+#define DEBOUNCE_TIME_MS (200 * portTICK_PERIOD_MS)
+
+#define BUTTON_GPIO		GPIO_NUM_35
+
+/**
+ * @brief return if the button is pressed or not
+ *
+ * @return		1 if pressed and 0 if not
+ */
+uint8_t button_is_pressed(void);
+
+/**
+ * @brief Init the gpio of the button and the ISR related
+ */
+void button_init(void);
+
+
+#endif /* BUTTON_E_PUCK2_H */
