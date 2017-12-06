567e02afcfa316f91c6231fd7e19a22788d17cfa
Eliot Ferragni <eliot.ferragni@epfl.ch>
2017-12-06 13:03:56 +0100

    -add of bluart_e-puck2 files

---
+++ Projects/ESP32_E-Puck_2/main/bluart_e-puck2.c
@@ -0,0 +1,11 @@
+/*
+
+File    : bluart_e-puck2.c
+Author  : Eliot Ferragni
+Date    : 6 december 2017
+REV 1.0
+
+Functions to init and use a bluetooth-UART translation
+*/
+
+#include "main_e-puck2.h"
---
+++ Projects/ESP32_E-Puck_2/main/bluart_e-puck2.h
@@ -0,0 +1,17 @@
+/*
+
+File    : bluart_e-puck2.h
+Author  : Eliot Ferragni
+Date    : 6 december 2017
+REV 1.0
+
+Functions to init and use a bluetooth-UART translation
+*/
+
+#ifndef BLUART_E_PUCK_2_H
+#define BLUART_E_PUCK_2_H
+
+
+
+
+#endif /* BLUART_E_PUCK_2_H */
