caa3fbc08099621159d4a6a2cf2f1f6ba5894b06
dburnier <danielburnier@gmail.com>
2022-03-15 16:07:58 +0100

    update

--- Projects/ESP32_Fishbot/main/project_infos.h
+++ Projects/ESP32_Fishbot/main/project_infos.h
@@ -2,6 +2,6 @@
     branch fishbot\n\
     commit 7a8e1ad01cd7074849b6527ff01d6b81dcc88155\n\
   Author: dburnier <danielburnier@gmail.com>\n\
-  Date:   Mon Feb 14 13:48:19 2022 +0100\n\
+  Date:   Wed Feb 16 13:06:19 2022 +0100\n\
 \n\
   Bluetooth to USART bridge allowing to communicate via Bluetooth with the SAMD uC of the Arduino"
